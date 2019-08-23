c8bc42cba4ebbb41491c6c0df42e199a031ac2df
Nazar <nvoloshchuk19@gmail.com>
2019-08-23 20:14:13 +0300

    api

--- tests/test_change_data.py
+++ tests/test_change_data.py
@@ -1,35 +1,103 @@
 import requests
 
-session = requests.Session()
 user_data = ("user@gmail.com", "user")
 data = {
-    "education": {"educationId": "2", "degree": "Master", "school": "KPI",
-                  "specialty": "Software Engineer", "graduation": 2009, },
-    "jobs": {"2": {"begin": "2010-04-08", "companyName": "InventorSoft", "description": "Senior Java Developer",
-                   "end": "2014-11-04", "jobId": 5, "position": "Senior", "printPdf": "true"},
-             "1": {"begin": "2008-03-08", "companyName": "ValSoft", "description": "Middle Java developer",
-                   "end": "2005-07-04", "jobId": 3, "position": "Middle", "printPdf": "true"},
-             "0": {"begin": "2006-10-08", "companyName": "SoftServe", "description": "Junior Java Developer",
-                   "end": "2009-10-04", "jobId": 4, "position": "Junior", "printPdf": "true"}},
-    "person": {"address": {"addressId": 1, "building": "20", "city": "Chernivtsi", "country": "Ukraine",
-                           "street": "Holovna", "zipCode": 58000},
-               "birthday": "1999-06-04",
-               "contact": {"contactId": 1, "email": "den.ohorodnik@gmail.com", "phoneNumber": "+380973999060"},
-               "firstName": "Denys", "lastName": "Ohorodnik", "user": {"userId": 3, "login": "user@gmail.com",
-                                                                       "password": "$2a$10$t31PsVNWl8eaWr9/gPwKKeX.4Q2grl12wmiRrN9fEZDMlMGHwA92m",
-                                                                       "enabled": "true"}},
-    "position": "Junior", "resumeId": 2, "reviewed": "false",
-    "skills": {"0": {"description": "Good skill", "printPdf": "true", "skillId": 3, "title": "Linux"},
-               "1": {"description": "Some experience", "printPdf": "true", "skillId": 4, "title": "Angular"},
-               "2": {"description": "Some experience", "printPdf": "true", "skillId": 5, "title": "Html"}}}
+    "resumeId": 2,
+    "position": "Middle Developer",
+    "skills": [
+        {
+            "skillId": 3,
+            "title": "Linux",
+            "description": "Good skill",
+            "printPdf": "true"
+        },
+        {
+            "skillId": 4,
+            "title": "Angular",
+            "description": "Some experience",
+            "printPdf": "true"
+        },
+        {
+            "skillId": 5,
+            "title": "Html",
+            "description": "Some experience",
+            "printPdf": "true"
+        }
+    ],
+    "jobs": [
+        {
+            "jobId": 4,
+            "position": "Junior",
+            "begin": "2006-10-08",
+            "end": "2009-10-04",
+            "companyName": "SoftServe",
+            "description": "Junior Java Developer",
+            "printPdf": "true"
+        },
+        {
+            "jobId": 5,
+            "position": "Senior",
+            "begin": "2010-04-08",
+            "end": "2014-11-04",
+            "companyName": "InventorSoft",
+            "description": "Senior Java Developer",
+            "printPdf": "true"
+        },
+        {
+            "jobId": 3,
+            "position": "Middle",
+            "begin": "2008-03-08",
+            "end": "2005-07-04",
+            "companyName": "ValSoft",
+            "description": "Middle Java developer",
+            "printPdf": "true"
+        }
+    ],
+    "education": {
+        "educationId": 2,
+        "degree": "Master",
+        "school": "KPI",
+        "specialty": "Software Engineer",
+        "graduation": 2009
+    },
+    "person": {
+        "userId": 3,
+        "firstName": "Denys",
+        "lastName": "Ohorodnik",
+        "birthday": "1999-06-04",
+        "contact": {
+            "contactId": 1,
+            "email": "den.ohorodnik@gmail.com",
+            "phoneNumber": "+380973999060"
+        },
+        "address": {
+            "addressId": 1,
+            "country": "Ukraine",
+            "city": "Chernivtsi",
+            "street": "Holovna",
+            "building": "20",
+            "zipCode": 58000
+        },
+        "user": {
+            "userId": 3,
+            "login": "user@gmail.com",
+            "password": "$2a$10$t31PsVNWl8eaWr9/gPwKKeX.4Q2grl12wmiRrN9fEZDMlMGHwA92m",
+            "enabled": "true"
+        }
+    },
+    "reviewed": "false",
+    "vacancies": [
+
+    ]
+}
 
 
 def test_change_data():
-    session.post('http://localhost:8080/RabotyNET/login', auth=user_data)
-    session.get('http://localhost:8080/RabotyNET/pdf')
-    session.get(url="http://localhost:8080/RabotyNET/users/3")
-    tmp = {"X-XSRF-TOKEN": session.cookies.get_dict()['XSRF-TOKEN']}
-    change = session.put('http://localhost:8080/RabotyNET/pdf/updatePDF', headers=tmp,
-                         json=data)
+    session = requests.Session()
+    session.post(url="http://localhost:8080/RabotyNET/login",
+                 auth=user_data)
+    session.get("http://localhost:8080/RabotyNET")
+    tmp = {"X-XSRF-TOKEN": session.cookies.get_dict()['XSRF-TOKEN'], 'content-type': 'application/json'}
+    change = session.put('http://localhost:8080/RabotyNET/pdf/updatePDF', json=data, headers=tmp)
 
-    print(change.text)
+    print(change.status_code)
