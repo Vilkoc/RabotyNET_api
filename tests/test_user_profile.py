b179fe6b4b9bd37dd357f84155a2c5513f8422e3
Dmytro Babyuk <d_babyuk@yahoo.com>
2019-08-24 17:52:47 +0300

    check_200 included

--- tests/test_user_profile.py
+++ tests/test_user_profile.py
@@ -1,23 +1,25 @@
 """ User Profile test"""
-from base import USER_PROFILE_URL, USER_URL
+from base import USER_PROFILE_URL, USER_URL, LOGOUT_URL
 import allure
 from credentials import Credentials
 from data_tests.user_data import updated_data
+
 
 @allure.feature('Getting user data')
 def test_user_profile_get(app):
     """Checking if user profile info is extracted"""
     with allure.step('Sign in and get user profile info'):
         app.authentication(*Credentials['USER'])
-        response_after_get = app.get(USER_PROFILE_URL)
-        assert response_after_get.status_code == 200, "Wrong status code"
-
+        app.get(USER_PROFILE_URL)
+        app.check_200()
+        #app.get(LOGOUT_URL)
 
 @allure.feature('Updating user data')
 def test_user_profile_update(app):
     """Checking if user profile info is updated"""
     with allure.step('Sign in and update user profile'):
         app.authentication(*Credentials['USER'])
-        response_after_put = app.put(USER_URL, data=updated_data)
-        assert response_after_put.status_code == 200, "Wrong status code"
+        app.put(USER_URL, data=updated_data)
+        app.check_200()
+        #app.get(LOGOUT_URL)
 
