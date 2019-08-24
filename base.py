ed23fc6a3b2d97a3914dffe376662dfce8de3e1d
natalyshevchuk <nshvchk24@gmail.com>
2019-08-24 13:23:16 +0300

    so close

--- base.py
+++ base.py
@@ -8,3 +8,7 @@ LOGOUT_URL = BASE_URL + 'logout'
 USER_REGISTER_URL = BASE_URL + 'users/auth'
 USER_CONFIRM_EMAIL_URL = BASE_URL + 'users/auth/confirm'
 
+#Companies
+COMPANY_EXISTS_URL = BASE_URL + 'companies/exists/InventorSoft'
+DELETING_OF_COMPANY = BASE_URL + 'companies/delete/2'
+UPDATE_COMPANY_URL = BASE_URL + 'companies/update'
