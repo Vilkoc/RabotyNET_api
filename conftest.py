d5c53fbbcebfcd0c06424ede3b43fe7c184845ce
Mykhailo <43335891+mykhailokokun@users.noreply.github.com>
2019-08-28 16:51:45 +0300

    Update conftest.py
    1

--- conftest.py
+++ conftest.py
@@ -3,10 +3,10 @@ from application import Application
 from utilities.db import prepare_db
 
 
-@pytest.fixture(scope='session', autouse=False)
-def prep_db(worker_id):
-    if worker_id == 'gw0' or worker_id == 'master':
-        prepare_db()
+# @pytest.fixture(scope='session', autouse=False)
+# def prep_db(worker_id):
+#     if worker_id == 'gw0' or worker_id == 'master':
+#         prepare_db()
 
 
 @pytest.fixture(scope='function')
