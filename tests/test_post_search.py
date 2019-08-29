c261fa168b5703fa92bf6a1279d39eea939e15dd
mykhailokokun <mykhailokokun@gmail.com>
2019-08-29 17:38:04 +0300

    no need to push

--- tests/test_post_search.py
+++ tests/test_post_search.py
@@ -7,8 +7,8 @@ from data_tests.guest_data import HEADERS
 
 
 @allure.feature("Search")
-@pytest.mark.skip(reason='Problems with the database')
-def search(app):
+# @pytest.mark.skip(reason='patamushto-because')
+def test_zsearch(app):
     """testing providing search"""
     with allure.step('Providing search for vacancy'):
         app.post(SEARCH_VACANCY_URL, data=SEARCH_VACANCY_DATA, headers=HEADERS)
